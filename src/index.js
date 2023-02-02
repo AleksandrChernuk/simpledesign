7924d7a7630bae364d1f0514d5d4985e29ffb094
CHerniuk <119289731+AleksandrChernuk@users.noreply.github.com>
2023-02-02 11:34:31 +0200

    accordion

--- src/index.js
+++ src/index.js
@@ -2,12 +2,16 @@ const btnUpDown = document.querySelectorAll(".accordion-btn");
 const accText = document.querySelectorAll(".accordion-text");
 const accContainer = document.querySelectorAll(".accordion-items");
 
-btnUpDown.forEach((e) =>
-  e.addEventListener("click", (e) => {
-    accContainer.forEach((e) => e.classList.toggle("closed"));
+// btnUpDown.forEach((e) =>
+//   e.addEventListener("click", (e) => {
+//     accContainer.forEach((e) => e.classList.toggle("closed"));
 
-    accText.forEach((e) => {
-      e.classList.toggle("show");
-    });
-  }),
-);
+//     accText.forEach((e) => {
+//       e.classList.toggle("show");
+//     });
+//   }),
+// );
+for (let i = 0; i < accContainer.length; i += 1) {
+  const element = accContainer[i];
+  element.addEventListener("click", () => element.classList.toggle("activ"));
+}
