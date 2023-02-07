af4400a7ae2a2041c594fa976645f692e04c4caf
CHerniuk <119289731+AleksandrChernuk@users.noreply.github.com>
2023-02-07 15:44:10 +0200

    webpack

--- src/index.js
+++ src/index.js
@@ -1,6 +1,2 @@
-const accContainer = document.querySelectorAll(".accordion-items");
-
-for (let i = 0; i < accContainer.length; i += 1) {
-  const element = accContainer[i];
-  element.addEventListener("click", () => element.classList.toggle("activ"));
-}
+import "./index.html";
+import "./style.css";
---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+
+module.exports = {
+  entry: "./src/index.js",
+  mode: "development",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "public"),
+    },
+    compress: true,
+    port: 9000,
+  },
+  plugins: [new HtmlWebpackPlugin({ template: "./src/index.html" })],
+  module: {
+    rules: [
+      {
+        test: /\.html$/i,
+        loader: "html-loader",
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+};
