11b7982cf558c4e2e4d5a9c182ee6047a6f363fb
CHerniuk <119289731+AleksandrChernuk@users.noreply.github.com>
2023-02-08 01:01:14 +0200

    parcel

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
-import "./index.html";
-import "./index.scss";
+const accContainer = document.querySelectorAll(".accordion-items");
 
-import accordion from "./component/accordion";
+for (let i = 0; i < accContainer.length; i += 1) {
+  const element = accContainer[i];
+  element.addEventListener("click", () => element.classList.toggle("activ"));
+}
